08f6140be2bd55c721a99ae2927b466bfbae48d8
Tracy0115 <shuai.yang@autodesk.com>
2015-06-17 15:34:39 +0800

    web.admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-# from django.contrib import admin
+from django.contrib import admin
 from mysite.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+	(r'^admin/', include(admin.site.urls)),
 	('^hello/$', hello),
 	('^stone/$', stone),
 	('^time/$', Current_Time),
